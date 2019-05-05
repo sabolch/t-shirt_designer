fe64e5db6c3bf32e6b675db861f398f5ef6120d8
Sabee <vargha.sabolch@gmail.com>
2019-05-05 22:17:07 +0200

    database error fix

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 
 return [
 
@@ -45,30 +45,30 @@ return [
             'prefix' => '',
         ],
 
-//        'mysql' => [
-//            'driver' => 'mysql',
-//            'host' => env('DB_HOST', '127.0.0.1'),
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
-//            'unix_socket' => env('DB_SOCKET', ''),
-//            'charset' => 'utf8mb4',
-//            'collation' => 'utf8mb4_unicode_ci',
-//            'prefix' => '',
-//            'strict' => true,
-//            'engine' => null,
-//        ],
-        'mysql' => array(
-            'driver'    => 'mysql',
-            'host'      => $host,
-            'database'  => $database,
-            'username'  => $username,
-            'password'  => $password,
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ),
+       'mysql' => [
+           'driver' => 'mysql',
+           'host' => env('DB_HOST', '127.0.0.1'),
+           'port' => env('DB_PORT', '3306'),
+           'database' => env('DB_DATABASE', 'forge'),
+           'username' => env('DB_USERNAME', 'forge'),
+           'password' => env('DB_PASSWORD', ''),
+           'unix_socket' => env('DB_SOCKET', ''),
+           'charset' => 'utf8mb4',
+           'collation' => 'utf8mb4_unicode_ci',
+           'prefix' => '',
+           'strict' => true,
+           'engine' => null,
+       ],
+        // 'mysql' => array(
+        //     'driver'    => 'mysql',
+        //     'host'      => $host,
+        //     'database'  => $database,
+        //     'username'  => $username,
+        //     'password'  => $password,
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        // ),
 
         'pgsql' => [
             'driver' => 'pgsql',
---
+++ database/migrations/2019_05_05_201231_create_color_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('color');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colors');
+    }
+}
